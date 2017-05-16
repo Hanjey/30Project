83e6e7e34d3300369da55ef2ab72a5c975981f95
Hanjey <zhunxun@gmail.com>
2017-05-16 14:20:45 +0800

    walk process pagetable

--- walk_qemu_pg/walk_process.c
+++ walk_qemu_pg/walk_process.c
@@ -6,6 +6,7 @@
 #include <linux/mm.h>
 #include <asm/pgtable.h>
 
+
 #define long_to_pfn(val) val>>12
 /*get target process struct  by pid*/
 static int getTargetProcess(struct task_struct **tprocess,int pid){
@@ -29,13 +30,19 @@ static int getTargetProcess(struct task_struct **tprocess,int pid){
 #define huge_page_1g(pud) (pud_val(pud)>>0x7)&0x1
 #define huge_page_2m(pmd) (pmd_val(pmd)>>0x7)&0x1
 
-#define is_pmd_ram(x) (pmd_val(x) & _PAGE_PRESENT)
-#define is_pte_ram(x) (pte_val(x) & _PAGE_PRESENT)
+#define PAGE_PRESENT_CHEN 0X1
+#define is_pmd_ram(x) (pmd_val(x) & PAGE_PRESENT_CHEN)
+#define is_pte_ram(x) (pte_val(x) & PAGE_PRESENT_CHEN)
 static int walk_page_table(struct task_struct *p){
 	pgd_t *pgd;
 	pud_t *pud;
 	pmd_t *pmd;
 	pte_t *pte;
+	int count_4k=0;                                                                                                                      
+	int count_4k_ram=0;                                                                                                                  
+    int count_2m=0;                                                                                                                      
+    int count_2m_ram=0;                                                                                                                   
+    int count_1g=0;
 	int count_pg=0,count_pu=0,count_pm=0,count_pt=0;
 	if(p->mm==NULL)
 		return -1;
@@ -43,30 +50,29 @@ static int walk_page_table(struct task_struct *p){
 		return -1;
 	pgd=p->mm->pgd;
 	printk("page table base:%p\n",pgd);
-	int count_4k=0;
-	int count_2m=0;
-	int count_1g=0;
 	while(count_pg<511){
 		if(!pgd_none(*pgd)){
+			printk("pgd:%lu\n",pgd_val(*pgd));
 			pud=(pud_t *)pgd_page_vaddr(*pgd);
-			printk("pud:%p\n",pud);
 			/*walk for one pgd*/
 			while(count_pu<511){
 			//	pud=(pud_t *)pgd_page_vaddr(*pgd_temp);
-				if((pud!=NULL)&&(!pud_none(*pud))){
+				if((!pud_none(*pud))){
+					printk("pud:%lu\n",pud_val(*pud));
 					pmd=(pmd_t *)pud_page_vaddr(*pud);
-					printk("pmd:%p\n",pmd);
 					/*walk for one pud*/
 					while(count_pm<511){
 					//	pmd=(pmd_t *)pud_page_vaddr(*pud_temp);
-						if((pmd!=NULL)&&(!pmd_none(*pmd))){
+						if((!pmd_none(*pmd))&&(!pmd_large(*pmd))){
+							printk("pmd:%lu\n",pmd_val(*pmd));
 							pte=(pte_t *)page_address(pmd_page(*(pmd)));
-							printk("pmd:%p\n",pmd);
 							/*walk for one pmd*/
 							while(count_pt<511){
 							 //	pte=(pte_t *)page_address(pmd_page(*(pmd)));
-								if((pte!=NULL)&&(!pte_none(*pte))&&pte_present(*pte)&&is_pte_ram(*pte)){
-									printk("pte:%p\n",pte);
+								if((!pte_none(*pte))){
+									if(is_pte_ram(*pte))
+										count_4k_ram++;
+									printk("pte:%lu\n",pte_val(*pte));
 									/*record 4K size page*/
 									count_4k++;
 								}
@@ -74,7 +80,10 @@ static int walk_page_table(struct task_struct *p){
 								count_pt++;
 							}
 						}
-						if((huge_page_2m(*pmd)==1)&&(pmd_present(*pmd))&&is_pmd_ram(*pmd)){
+						/*if pmd point to a 2m page and is in ram*/
+						if((!pmd_none(*pmd)&&pmd_large(*pmd))){
+							if(is_pmd_ram(*pmd))
+									count_2m_ram++;
 							count_2m++;
 							/*record 2M size page*/
 						}
@@ -82,7 +91,7 @@ static int walk_page_table(struct task_struct *p){
 						count_pm++;
 					}
 				}
-				if(huge_page_1g(*pud)==1){
+				if((huge_page_1g(*pud))==1){
 					count_1g++;
 					//记录1G大小的页面
 				}
@@ -94,7 +103,9 @@ static int walk_page_table(struct task_struct *p){
 		count_pg++;
 	}
 	printk("4k page count:%d\n",count_4k);
+	printk("4k page in ram count:%d\n",count_4k_ram); 
 	printk("2m page count:%d\n",count_2m);
+	printk("2m page in ram count:%d\n",count_2m_ram); 
 	printk("1g page count:%d\n",count_1g);
 	return 0;
 }
